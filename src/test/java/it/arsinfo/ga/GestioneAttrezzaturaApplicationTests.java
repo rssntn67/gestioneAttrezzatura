347dba2f29c1613f9b2de96cb5fb34ea59793941
Antonio <rssntn67@yahoo.it>
2020-06-29 18:05:17 +0200

    dded test files and properties

---
+++ src/test/java/it/arsinfo/ga/GestioneAttrezzaturaApplicationTests.java
@@ -0,0 +1,21 @@
+package it.arsinfo.ga;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestioneAttrezzaturaApplicationTests {
+    
+    @Before    
+    public void setUp() {        
+    }
+    
+    @After
+    public void clearDown() {
+    }
+    
+}
---
+++ src/test/java/it/arsinfo/ga/GestioneAttrezzaturaUnitTests.java
@@ -0,0 +1,10 @@
+package it.arsinfo.ga;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class GestioneAttrezzaturaUnitTests {
+    
+
+}
